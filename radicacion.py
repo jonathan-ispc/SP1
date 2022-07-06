d92a358766156df581c4e595dad905093ad9fa0c
Jonathan-ispc <jonathanbustos.ispc@gmail.com>
2022-07-06 12:57:23 -0300

    commit radicacion

--- radicacion.py
+++ radicacion.py
@@ -1,13 +1,9 @@
 # Función radicación: retorna la raíz del primero respecto del segundos parámetros Args: base indice 
     
-def radicacion (base, indice):
-
-    return pow(base, 1/indice )
-
-a=int(input("Ingrese la base:"))
-b=int(input("Ingrese el índice":"))
+base=int(input("Ingrese la base: "))
+indice=int(input("Ingrese el índice: "))
 
 def radicacion (a, b): 
     return pow(base, 1/indice)
 
-print("La radicación es:", radicación(a, b))
+print("La radicación es:", radicacion(base, indice))
