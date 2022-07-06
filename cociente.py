c5a2de9b4c74ad2b872f1c5927eaf2ad37f25be8
Jonathan-ispc <jonathanbustos.ispc@gmail.com>
2022-07-06 12:34:07 -0300

    commit cociente

---
+++ cociente.py
@@ -0,0 +1,8 @@
+# Función cociente: retorna el cociente de 2 parámetros.
+
+a=int(input("Ingrese un número:"))
+b=int(input("Ingrese un número:"))
+def cociente (a, b): 
+    return a / b
+
+print("El cociente es:", cociente(a, b))
