4d3dd8ad02e899ebf6cf381821b803b511a21a29
Jonathan-ispc <jonathanbustos.ispc@gmail.com>
2022-07-06 12:48:08 -0300

    commit potencia

---
+++ potencia.py
@@ -0,0 +1,7 @@
+# Función potencia: retorna la  retorna la potencia del primero elevado al segundo parámetros
+a=int(input("Ingrese un número: "))
+b=int(input("Ingrese la potencia: "))
+def potencia (a, b): 
+    return a ** b
+
+print("La potencia es:", potencia(a, b))
