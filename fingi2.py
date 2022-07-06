400a6d516bcd69615353c15e01aa37a586958744
Jonathan-ispc <jonathanbustos.ispc@gmail.com>
2022-07-06 11:58:33 -0300

    commit.b

--- fingi2.py
+++
@@ -1,6 +0,0 @@
-# Función ing2i : Permite el ingreso de 2 valores enteros
-
-def ing2i():
-    a= int(input("Ingrese un número entero"))
-    b= int(input("Ingrese el segundo número entero"))
-    return [a,b]
